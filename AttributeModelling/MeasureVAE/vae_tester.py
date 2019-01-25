Bug fix in note_range attribute computation. Minor refactor of plot_surface method

AttributeModelling/MeasureVAE/vae_tester.py
@@ -156,18 +156,17 @@ class VAETester(object):
             mean_accuracy
         )
 
-    def plot_attribute_surface(self, attribute='rhy_complexity', dim1=0, dim2=1, grid_res=0.5):
+    def plot_attribute_surface(self, dim1=0, dim2=1, grid_res=0.5):
         """
         Plots the value of an attribute over a surface defined by the dimensions
-        :param attribute: str,
         :param dim1: int,
         :param dim2: int,
         :param grid_res: float,
         :return:
         """
         # create the dataspace
-        x1 = torch.arange(-1.0, 1.0, grid_res)
-        x2 = torch.arange(-1.0, 1.0, grid_res)
+        x1 = torch.arange(-0.5, 0.5, grid_res)
+        x2 = torch.arange(-0.5, 0.5, grid_res)
         z1, z2 = torch.meshgrid([x1, x2])
         num_points = z1.size(0) * z1.size(1)
         z = torch.randn(num_points, self.model.latent_space_dim)
@@ -175,9 +174,12 @@ class VAETester(object):
         z[:, dim2] = z2.contiguous().view(1, -1)
         z = to_cuda_variable(z)
         # pass the points through the model decoder
-        mini_batch_size = 400
+        mini_batch_size = 500
         num_mini_batches = num_points // mini_batch_size
-        attr_all = []
+        nd_all = []
+        nr_all = []
+        rc_all = []
+        ie_all = []
         for i in range(num_mini_batches):
             print(i)
             z_batch = z[i*mini_batch_size:(i+1)*mini_batch_size, :]
@@ -188,25 +190,31 @@ class VAETester(object):
                 train=self.train
             )
             samples = samples.view(z_batch.size(0), -1)
-            if attribute == 'num_notes':
-                attr = self.dataset.get_num_notes_in_measure(samples)
-            elif attribute == 'note_range':
-                attr = self.dataset.get_note_range_of_measure(samples)
-            elif attribute == 'rhy_entropy':
-                attr = self.dataset.get_rhythmic_entropy(samples)
-            elif attribute == 'beat_strength':
-                attr = self.dataset.get_beat_strength(samples)
-            elif attribute == 'rhy_complexity':
-                attr = self.dataset.get_rhy_complexity(samples)
-            else:
-                raise ValueError('Invalid attribute type')
-            attr_all.append(attr)
-        attr_all = torch.cat(attr_all, 0)
+            note_density = self.dataset.get_notes_density_in_measure(samples)
+            note_range = self.dataset.get_note_range_of_measure(samples)
+            rhy_complexity = self.dataset.get_rhy_complexity(samples)
+            interval_entropy = self.dataset.get_interval_entropy(samples)
+            nd_all.append(note_density)
+            nr_all.append(note_range)
+            rc_all.append(rhy_complexity)
+            ie_all.append(interval_entropy)
+        nd_all = to_numpy(torch.cat(nd_all, 0))
+        nr_all = to_numpy(torch.cat(nr_all, 0))
+        rc_all = to_numpy(torch.cat(rc_all, 0))
+        ie_all = to_numpy(torch.cat(ie_all, 0))
         z = to_numpy(z)
-        attr_val = to_numpy(attr_all)
-        filename = self.dir_path + '/plots/attr_surf_' + attribute + '_[' \
+        filename = self.dir_path + '/plots/attr_surf_note_density_[' \
                    + str(dim1) + ',' + str(dim2) + '].png'
-        self.plot_dim(z, attr_val, filename, dim1=dim1, dim2=dim2)
+        self.plot_dim(z, nd_all, filename, dim1=dim1, dim2=dim2)
+        filename = self.dir_path + '/plots/attr_surf_note_range_[' \
+                   + str(dim1) + ',' + str(dim2) + '].png'
+        self.plot_dim(z, nr_all, filename, dim1=dim1, dim2=dim2)
+        filename = self.dir_path + '/plots/attr_surf_rhy_complexity_[' \
+                   + str(dim1) + ',' + str(dim2) + '].png'
+        self.plot_dim(z, rc_all, filename, dim1=dim1, dim2=dim2)
+        filename = self.dir_path + '/plots/attr_surf_int_entropy_[' \
+                   + str(dim1) + ',' + str(dim2) + '].png'
+        self.plot_dim(z, ie_all, filename, dim1=dim1, dim2=dim2)
 
     def plot_attribute_dist(self, attribute='num_notes', plt_type='pca'):
         """
@@ -235,7 +243,7 @@ class VAETester(object):
             z_tilde = z_dist.loc
             z_all.append(z_tilde)
             if attribute == 'num_notes':
-                attr = self.dataset.get_num_notes_in_measure(score_tensor)
+                attr = self.dataset.get_notes_density_in_measure(score_tensor)
             elif attribute == 'note_range':
                 attr = self.dataset.get_note_range_of_measure(score_tensor)
             elif attribute == 'rhy_entropy':
@@ -353,8 +361,10 @@ class VAETester(object):
             x=data[:, dim1],
             y=data[:, dim2],
             c=target,
+            s=12,
+            linewidths=0,
             cmap="viridis",
-            alpha=0.3
+            alpha=0.8
         )
         plt.colorbar()
         plt.savefig(filename, format='png', dpi=300)

AttributeModelling/data/dataloaders/bar_dataset.py
@@ -8,6 +8,7 @@ from scipy import stats
 from abc import ABC, abstractmethod
 from joblib import Parallel, delayed
 import multiprocessing
+import torch.nn.functional as F
 
 from music21 import meter
 from music21.abcFormat import ABCHandlerException
@@ -437,7 +438,7 @@ class FolkBarDataset:
         return score
 
     # Measure attribute extractors
-    def get_num_notes_in_measure(self, measure_tensor):
+    def get_notes_density_in_measure(self, measure_tensor):
         """
         Returns the number of notes in each measure of the input normalized by the
         length of the length of the measure representation
@@ -468,36 +469,27 @@ class FolkBarDataset:
                 (batch_size)
         """
         batch_size, measure_seq_len = measure_tensor.size()
-        midi_min, midi_max = self.pitch_range
         index2note = self.index2note_dicts
         slur_index = self.note2index_dicts[SLUR_SYMBOL]
         rest_index = self.note2index_dicts['rest']
         none_index = self.note2index_dicts[None]
         start_index = self.note2index_dicts[START_SYMBOL]
         end_index = self.note2index_dicts[END_SYMBOL]
-        has_note = False
-        nrange = torch.zeros(batch_size)
-        if torch.cuda.is_available():
-            nrange = torch.autograd.Variable(nrange.cuda())
-        else:
-            nrange = torch.autograd.Variable(nrange)
+        nrange = to_cuda_variable(torch.zeros(batch_size))
         for i in range(batch_size):
-            low_midi = midi_max
-            high_midi = midi_min
+            midi_notes = []
             for j in range(measure_seq_len):
                 index = measure_tensor[i][j].item()
                 if index not in (slur_index, rest_index, none_index, start_index, end_index):
-                    has_note = True
-                    midi_j = music21.pitch.Pitch(index2note[index]).midi
-                    if midi_j <= low_midi:
-                        low_midi = midi_j
-                    if midi_j >= high_midi:
-                        high_midi = midi_j
-                if has_note:
-                    nrange[i] = high_midi - low_midi
-                else:
-                    nrange[i] = -0.1
-        return nrange.float() / (midi_max - midi_min)
+                    midi_note = torch.Tensor([music21.pitch.Pitch(index2note[index]).midi])
+                    midi_notes.append(midi_note)
+            if len(midi_notes) == 0 or len(midi_notes) == 1:
+                nrange[i] = 0
+            else:
+                midi_notes = torch.cat(midi_notes, 0)
+                midi_notes = to_cuda_variable_long(midi_notes)
+                nrange[i] = torch.max(midi_notes) - torch.min(midi_notes)
+        return nrange / 26
 
     def get_rhythmic_entropy(self, measure_tensor):
         """
@@ -577,6 +569,47 @@ class FolkBarDataset:
         h_product = weights * beat_tensor
         b_str = torch.sum(h_product, dim=1) / norm_coeff
         return b_str
+
+    def get_interval_entropy(self, measure_tensor):
+        """
+        Returns the normalized interval entropy of a batch of measures
+        :param measure_tensor: torch Variable,
+                (batch_size, measure_seq_len)
+        :return: torch Variable,
+                (batch_size)
+        """
+        batch_size, measure_seq_len = measure_tensor.size()
+        index2note = self.index2note_dicts
+        slur_index = self.note2index_dicts[SLUR_SYMBOL]
+        rest_index = self.note2index_dicts['rest']
+        none_index = self.note2index_dicts[None]
+        start_index = self.note2index_dicts[START_SYMBOL]
+        end_index = self.note2index_dicts[END_SYMBOL]
+        has_note = False
+        nrange = to_cuda_variable(torch.zeros(batch_size))
+        for i in range(batch_size):
+            midi_notes = []
+            for j in range(measure_seq_len):
+                index = measure_tensor[i][j].item()
+                if index not in (slur_index, rest_index, none_index, start_index, end_index):
+                    midi_note = torch.Tensor([music21.pitch.Pitch(index2note[index]).midi])
+                    midi_notes.append(midi_note)
+            if len(midi_notes) == 0 or len(midi_notes) == 1:
+                nrange[i] = 0
+            else:
+                midi_notes = torch.cat(midi_notes, 0)
+                midi_notes = to_cuda_variable_long(midi_notes)
+                # compute intervals
+                midi_notes = torch.abs(midi_notes[1:] - midi_notes[:-1])
+                midi_notes = midi_notes % 12
+                probs = to_cuda_variable(torch.zeros([12, 1]))
+                for k in range(len(midi_notes)):
+                    probs[midi_notes[k]] += 1
+                # compute entropy of this interval vector
+                b = F.softmax(probs, dim=0) * F.log_softmax(probs, dim=0)
+                b = -1.0 * b.sum()
+                nrange[i] = b
+        return nrange
 
 
 class FolkNBarDataset(FolkBarDataset):
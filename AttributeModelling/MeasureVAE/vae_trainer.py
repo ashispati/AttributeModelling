0b9b778b2722fe394465f9f19d4cf3cf053850bc
ashispati <ashis.pati@gatech.edu>
2019-03-02 13:57:14 -0500

    Added training config for joint training of rhythmic complexity and pitch range

--- AttributeModelling/MeasureVAE/vae_trainer.py
+++ AttributeModelling/MeasureVAE/vae_trainer.py
@@ -24,6 +24,8 @@ class VAETrainer(Trainer):
             self.reg_dim = reg_dim
             if self.reg_type == 'joint':
                 self.reg_dim = [0, 1]
+            if self.reg_type == 'joint_rhycomp_noterange':
+                self.reg_dim = [0, 2]
             self.trainer_config = '[' + self.reg_type + ',' + str(self.reg_dim) + ']'
             self.model.update_trainer_config(self.trainer_config)
         self.warm_up_epochs = 10
@@ -132,6 +134,13 @@ class VAETrainer(Trainer):
         elif self.reg_type == 'joint':
             attr1_tensor = self.dataset.get_rhy_complexity(score)
             attr2_tensor = self.dataset.get_notes_density_in_measure(score)
+            x1 = z[:, self.reg_dim[0]]
+            x2 = z[:, self.reg_dim[1]]
+            reg_loss = self.reg_loss_sign(x1, attr1_tensor) + self.reg_loss_sign(x2, attr2_tensor)
+            return reg_loss
+        elif self.reg_type == 'joint_rhycomp_noterange':
+            attr1_tensor = self.dataset.get_rhy_complexity(score)
+            attr2_tensor = self.dataset.get_note_range_of_measure(score)
             x1 = z[:, self.reg_dim[0]]
             x2 = z[:, self.reg_dim[1]]
             reg_loss = self.reg_loss_sign(x1, attr1_tensor) + self.reg_loss_sign(x2, attr2_tensor)

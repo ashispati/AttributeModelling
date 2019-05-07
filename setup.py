1a3e2d8f33d1a2adc48b95d0d898ab3e2dd8144a
Ashis <ashis.pati@gatech.edu>
2019-05-07 17:32:26 -0400

    Added additional dependencies

--- setup.py
+++ setup.py
@@ -3,5 +3,5 @@ from setuptools import setup
 setup(
     name="AttributeModelling",
     packages=['AttributeModelling'],
-    install_requires=['pymongo', 'music21', 'sshtunnel', 'numpy', 'torch', 'tqdm', 'click']
+    install_requires=['pymongo', 'music21', 'sshtunnel', 'numpy', 'torch', 'tqdm', 'click', 'tensorboard-logger', 'scikit-learn', 'scipy', 'matplotlib']
 )
